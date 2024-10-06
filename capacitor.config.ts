9a5cfb19b1b865fbb22405e333bcec5846eea9c6
Felipe Fernandes <105753904+lFelipeFernandesl@users.noreply.github.com>
2024-10-06 16:53:27 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Segunda Competencia',
+  webDir: 'www'
+};
+
+export default config;
